ecd2f551de46b25f0b3a607a844518f49a490b81
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-08 20:49:09 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,6 +19,7 @@ def remove_strawberry(contacts)
     contact_details_hash.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
           binding.pry
+      end
     end
   end
 end
